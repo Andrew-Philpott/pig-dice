f761568fc55b875d99a9e63e5dc53ad01f04d6c3
Andrew Philpott <andrewphilpott92@gmail.com>
2019-11-06 17:35:51 -0800

    Change 'PigDie' to 'var'.

--- __tests__/pigdie.test.js
+++ __tests__/pigdie.test.js
@@ -2,8 +2,8 @@ import { PigDie } from './..src/pigdie.js';
 
 describe('PigDie', () => {
 
-  test('should correctly make a pigdie', () => {
-    PigDie pigdie = new PigDie(7);
+  test('should correctly make a pigdie with its number of sides set to the value provided', () => {
+    var pigdie = new PigDie(7);
     expect(pigdie.numberOfSides).toEqual(7);
   });
 });
